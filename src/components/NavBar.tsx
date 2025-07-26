72d68bd0b79be425606c1cc18b835a85471c2db3
erpovfan <er81cd81fan81@gmail.com>
2025-07-26 10:42:21 +0330

    navbar motion

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,38 @@
+"use client";
 import React from "react";
+
 import { navLinks } from "../../constants";
+import Image from "next/image";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function NavBar() {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      {
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "#00000050",
+        backdropFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
   return (
     <nav>
       <div>
         <a href="#home">
+          <Image src={"/images/logo.png"} alt="logo" width={30} height={30} />
           <p>Velvet pour</p>
         </a>
 
